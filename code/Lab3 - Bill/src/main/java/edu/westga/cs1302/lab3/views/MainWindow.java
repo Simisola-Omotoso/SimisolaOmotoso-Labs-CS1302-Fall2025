0fa05f752c56fc35062e406514a41d989b2b1cd9
Simisola E Omotoso <so00089@my.westga.edu>
2025-09-11 05:29:15 -0400

    Designed GUI
    1) Created GUI for MainWindow.fxml to use
    2) Focus on what the GUI should look like
    3) Connected SceneBuilder with fxml and built layout

--- code/Lab3 - Bill/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
+++ code/Lab3 - Bill/src/main/java/edu/westga/cs1302/lab3/views/MainWindow.java
@@ -1,5 +1,13 @@
 package edu.westga.cs1302.lab3.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import edu.westga.cs1302.lab3.model.Bill;
+import edu.westga.cs1302.lab3.model.BillItem;
+
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -8,11 +16,36 @@ package edu.westga.cs1302.lab3.views;
  * @version Fall 2025
  */
 public class MainWindow {
-    
+	
+	@FXML private TextField name;
+	@FXML private TextField amount;
+	@FXML private Button button;
+	@FXML private TextArea billDisplay;
+	
+	private Bill bill = new Bill();
+	private BillView view = new BillView();
+	
     /**
      * Perform any needed initialization of UI components and underlying objects.
      */
     public void initialize() {
+    	String name = this.name.getText();
+    	String amount = this.amount.getText();
+    	
+    	if (name.isEmpty() || amount.isEmpty()) {
+    		this.billDisplay.setText("Enter name & amount.");
+    		return;
+    	}
+    	
+    	try {
+    		double amountValue = Double.parseDouble(amount);
+        	BillItem item = new BillItem(name, amountValue);
+        	bill.addItem(item);
+        	String billView = view.getText(bill);
+        	billDisplay.setText(billView);
+    	} catch (NumberFormatException e) {
+    		this.billDisplay.setText("Amount is an invalid value.");
+    	}
     	
     }
 }
