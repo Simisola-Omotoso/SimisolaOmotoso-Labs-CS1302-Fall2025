d0c18dd4a31dc9cd8be513334dbc089ed8de85f8
Simi0M0 <somotoso@Outlook.com>
2025-09-11 13:02:01 -0400

    Created calculateSubtotal Method
    1) Created method to calculate subtotal of items
    2) To get subtotal of the entire items array
    3) Created new method with BillItem array, checked if it was null, and iterated through items to add subtotal together

--- code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -1,5 +1,21 @@
 package edu.westga.cs1302.bill.model;
 
 public class BillCalculator {
+	
+	public static double calculateSubtotal(BillItem[] items) {
+		if (items == null) {
+			throw new IllegalArgumentException("Items array can't be null.");
+		}
+		for (BillItem item : items) {
+			if (item == null) {
+				throw new IllegalArgumentException("Item can't be null.");
+			}
+		}
+		double subtotal = 0.0;
+		for (BillItem item : items) {
+			subtotal += item.getAmount();
+		}
+		return subtotal;
+	}
 
 }
