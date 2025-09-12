352cbddbd2f939f6936623c9745061b74c5c9e34
Simi0M0 <somotoso@Outlook.com>
2025-09-12 14:21:28 -0400

    Added calculateTotal Method
    1) Added another method to calculate everything
    2) To calculate the total cost of the array of items
    3) Copied previous structure of methods and copied the for loops of each previous method, then returned the total of those calculations

--- code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
+++ code/Lab4/src/edu/westga/cs1302/bill/model/BillCalculator.java
@@ -49,5 +49,29 @@ public class BillCalculator {
 		}
 		return totalTip;
 	}
+	
+	public static double calculateTotal(BillItem[] items) {
+		if (items == null) {
+			throw new IllegalArgumentException("Items array can't be null.");
+		}
+		for (BillItem item : items) {
+			if (item == null) {
+				throw new IllegalArgumentException("Item can't be null.");
+			}
+		}
+		double subtotal = 0.0;
+		for (BillItem item : items) {
+			subtotal += item.getAmount();
+		}
+		double totalTax = 0.0;
+		for (BillItem item : items) {
+			totalTax += item.getAmount() * 0.1;
+		}
+		double totalTip = 0.0;
+		for (BillItem item : items) {
+			totalTip += item.getAmount() * 0.2;
+		}
+		return subtotal + totalTax + totalTip;
+	}
 
 }
