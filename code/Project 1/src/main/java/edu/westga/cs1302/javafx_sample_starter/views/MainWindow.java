bed6a811a9ad61c101c9e61aba44ab8cd652709a
Simisola E Omotoso <so00089@my.westga.edu>
2025-09-20 14:44:17 -0400

    UI for Adding Tasks
    1) Added UI components and added fields for those components
    2) To connect UI to codebehind
    3) Entered components in SceneBuilder, saved it, and entered each editable component as a field in MainWindow.java

--- code/Project 1/src/main/java/edu/westga/cs1302/javafx_sample_starter/views/MainWindow.java
+++ code/Project 1/src/main/java/edu/westga/cs1302/javafx_sample_starter/views/MainWindow.java
@@ -1,5 +1,11 @@
 package edu.westga.cs1302.javafx_sample_starter.views;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -8,6 +14,21 @@ package edu.westga.cs1302.javafx_sample_starter.views;
  * @version Fall 2025
  */
 public class MainWindow {
+	
+	@FXML
+	private TextField taskNameField;
+	
+	@FXML
+	private TextArea taskDescriptionArea;
+	
+	@FXML
+	private ComboBox priorityDropdown;
+	
+	@FXML
+	private Button submit;
+	
+	@FXML
+	private ListView taskList;
     
     /**
      * Perform any needed initialization of UI components and underlying objects.
