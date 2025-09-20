8f35becfaa109e7acb73c79fafb4d3675c76ef21
Simi0M0 <somotoso@Outlook.com>
2025-09-20 13:40:35 -0400

    Added Project 1 Files (JavaFX Starter)
    1) Added JavaFX starter files to code folder
    2) To start Project 1
    3) Renamed JavaFX starter files to Project 1 and dropped the files into code folder

---
+++ code/Lab4/src/edu/westga/cs1302/bill/test/model/bill_calculator/TestCalculateSubtotal.java
@@ -0,0 +1,12 @@
+package edu.westga.cs1302.bill.test.model.bill_calculator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import edu.westga.cs1302.bill.model.BillItem;
+
+public class TestCalculateSubtotal {
+	
+
+}
