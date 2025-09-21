7bfd3490faa68c23044ea68918850f6daf91885e
Simisola E Omotoso <so00089@my.westga.edu>
2025-09-21 18:33:40 -0400

    Connect UI to Business Logic for Adding Task
    1) Added a new method to MainWindow for adding tasks to ListView
    2) To let TaskManager handle adding tasks
    3) Added new protected void method that creates new task and adds it to observableList for ListView as well as initializing the ComboBox

--- code/Project 1/src/main/java/edu/westga/cs1302/javafx_sample_starter/model/views/MainWindow.java
+++ code/Project 1/src/main/java/edu/westga/cs1302/javafx_sample_starter/model/views/MainWindow.java
@@ -1,4 +1,5 @@
 package edu.westga.cs1302.javafx_sample_starter.model.views;
+import edu.westga.cs1302.javafx_sample_starter.model.Task;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
@@ -6,6 +7,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  * Controller class for drawing various things to our canvas window.
@@ -22,18 +25,30 @@ public class MainWindow {
 	private TextArea taskDescriptionArea;
 	
 	@FXML
-	private ComboBox priorityDropdown;
+	private ComboBox<String> priorityDropdown;
 	
 	@FXML
 	private Button submit;
 	
 	@FXML
-	private ListView taskList;
+	private ListView<Task> taskList;
+	
+	private ObservableList<Task> tasks;
     
     /**
      * Perform any needed initialization of UI components and underlying objects.
      */
     public void initialize() {
+    	tasks = FXCollections.observableArrayList();
+    	taskList.setItems(tasks);
     	
+    	priorityDropdown.setItems(FXCollections.observableArrayList("High", "Medium", "Low"));
+    	priorityDropdown.setValue("Low");
     }
+    
+    @FXML
+    protected void addTask() {
+		Task newTask = new Task(taskNameField.toString(), taskDescriptionArea.toString(), priorityDropdown.toString());
+		tasks.add(newTask);
+	}
 }
