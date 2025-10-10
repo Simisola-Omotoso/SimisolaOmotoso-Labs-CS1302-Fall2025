1f735a6bcfb276c5778225212aa6d1ae3f95fe2e
Simisola E Omotoso <so00089@my.westga.edu>
2025-10-10 11:54:13 -0400

    Descending Comparison of Tasks
    1) New class called Descending that handles the sorting of tasks in descending order
    2) To sort tasks in descending order
    3) Added new class and implemented a method to compare two tasks and return certain values based on type of sort

---
+++ code/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/Descending.java
@@ -0,0 +1,34 @@
+package edu.westga.cs1302.task_tracker.model;
+
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
+/** Compares tasks in descending order
+ * 
+ * @author Simisola Omotoso
+ * @version 1.0
+ */
+
+public class Descending {
+	
+	/** Compares two tasks and returns corresponding integer
+	 * 
+	 * @param task1 first task
+	 * @param task2 second task
+	 * @return value that is associated with a certain case
+	 */
+	
+	public int compare(Task task1, Task task2) {
+		if (task1.getPriority() == TaskPriority.LOW && task2.getPriority() == TaskPriority.MEDIUM) {
+			return 1;
+		} else if (task1.getPriority() == TaskPriority.MEDIUM && task2.getPriority() == TaskPriority.HIGH) {
+			return 1;
+		} else if (task1.getPriority() == TaskPriority.MEDIUM && task2.getPriority() == TaskPriority.MEDIUM) {
+			return 0;
+		} else if (task1.getPriority() == TaskPriority.HIGH && task2.getPriority() == TaskPriority.HIGH) {
+			return 0;
+		} else if (task1.getPriority() == TaskPriority.LOW && task2.getPriority() == TaskPriority.LOW) {
+			return 0;
+		}
+		return -1;
+	}
+}
