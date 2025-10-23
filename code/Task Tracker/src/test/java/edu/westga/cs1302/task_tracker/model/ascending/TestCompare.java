3a13d1f0299504be2fd41b74027243faa08797fe
Simisola E Omotoso <so00089@my.westga.edu>
2025-10-23 12:11:59 -0400

    Added Project 2 Files
    1) New Task Tracker project
    2) To start Project 2
    3) Moved new Task Tracker folder over to code folder

--- code/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/ascending/TestCompare.java
+++ code/Task Tracker/src/test/java/edu/westga/cs1302/task_tracker/model/ascending/TestCompare.java
@@ -4,11 +4,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import edu.westga.cs1302.task_tracker.model.Ascending;
+import edu.westga.cs1302.task_tracker.model.Task;
+import edu.westga.cs1302.task_tracker.model.Task.TaskPriority;
+
 class TestCompare {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCompareNormal() {
+		Task task1 = new Task("Submit Lab 6", "Turn in lab 6 by due date.", TaskPriority.HIGH);
+		Task task2 = new Task("Work on Project 2", "Complete tasks associated with Project 2.", TaskPriority.MEDIUM);
+		Ascending ascending = new Ascending();
+		assertEquals(1, ascending.compare(task1, task2));
 	}
 
 }
