ff6c00414e128c7dcc7e84b6b63c4823b282b7d7
Simisola E Omotoso <so00089@my.westga.edu>
2025-11-16 11:53:37 -0500

    Enable/Disable Generate Password
    1) Generate password button now disables whenever an invalid number is typed in the minimum length text field
    2) To not allow the user to click on the button and generate errors
    3) Updated this.minimumLength.textProperty().addListener() to include an if statement that checks for whenever checkForInvalidMinimumLengthText() is false, the .setDisable method is set to true, and else, set to false

--- code/starterL9/L9 - Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
+++ code/starterL9/L9 - Starter/src/main/java/edu/westga/cs1302/password_generator/view/MainWindow.java
@@ -47,12 +47,17 @@ public class MainWindow {
     	
     	this.minimumLength.textProperty().addListener((observable, newValue, oldValue) -> {
     		this.minLengthErrorText.setVisible(this.vm.checkForInvalidMinimumLengthText(newValue));
+    		if (!this.vm.checkForInvalidMinimumLengthText(newValue)) {
+    			this.generatePasswordButton.setDisable(true);
+    		} else {
+    			this.generatePasswordButton.setDisable(false);
+    		}
     	});
     	
     	this.generatePasswordButton.setOnAction(
 			(event) -> { 
 				this.vm.generatePassword();
-			} 
+			}
     	);
     	
     	this.fileSave.setOnAction(
