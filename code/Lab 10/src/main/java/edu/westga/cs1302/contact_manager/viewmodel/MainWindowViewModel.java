07050737036dc951536baa148f7fe72c07230137
Simisola E Omotoso <so00089@my.westga.edu>
2025-12-01 09:59:50 -0500

    Prevent Duplicate Contacts
    1) For loop within addContact that loops through contacts
    2) To search for duplicate contacts
    3) Added for loop that loops through contact, and if both contacts have the same phone number or name, error message

--- code/Lab 10/src/main/java/edu/westga/cs1302/contact_manager/viewmodel/MainWindowViewModel.java
+++ code/Lab 10/src/main/java/edu/westga/cs1302/contact_manager/viewmodel/MainWindowViewModel.java
@@ -37,7 +37,7 @@ public class MainWindowViewModel {
 		this.phoneNumber = new SimpleStringProperty("");
 		this.searchCriteria = new SimpleStringProperty("");
 		this.contacts = new SimpleListProperty<Contact>(FXCollections.observableList(new ArrayList<Contact>()));
-		this.phoneNumber = new HashMap<String, Contact>();
+		this.phoneNumberMap = new HashMap<String, Contact>();
 		this.nameMap = new HashMap<String, Contact>();
 	}
 	
@@ -94,6 +94,11 @@ public class MainWindowViewModel {
 	 */
 	public void addContact() throws IllegalArgumentException {
 		Contact contact = new Contact(this.name.get(), this.phoneNumber.get());
+		for (Contact newContact : this.contacts) {
+			if (newContact.getName().equals(contact.getName()) || newContact.getPhoneNumber().equals(contact.getPhoneNumber())) {
+				throw new IllegalArgumentException("Duplicate contact");
+			}
+		}
 		this.contacts.add(contact);
 		this.phoneNumberMap.put(contact.getPhoneNumber(), contact);
 		this.phoneNumberMap.put(contact.getName(), contact);
@@ -122,12 +127,6 @@ public class MainWindowViewModel {
 				return contact.toString();
 			}
 		}
-		/*
-		for (Contact currContact : this.contacts.get()) {
-			if (currContact.getName().equals(this.searchCriteria.get()) || currContact.getPhoneNumber().equals(this.searchCriteria.get())) {
-				return currContact.toString();
-			}
-		}*/
 		return "No contact found.";
 	}
 	
